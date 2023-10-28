8be9fb24c100be0e74480e28dd739ffe7bc5c641
Prabhav <122600613+sprabhav7@users.noreply.github.com>
2023-10-28 15:24:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,50 @@
 import importlib
 import importlib.metadata
 import requests
-from metadata_validator import MetadataAnalyzer
-from metadata_parser import MetadataInfo
 from bs4 import BeautifulSoup
-
-'''
-def fetch_package_metadata(package_name):
-    url = f"https://pypi.org/pypi/{package_name}/json"
-    response = requests.get(url)
-    response.raise_for_status()
-
-    metadata = response.json()
-    return metadata
+from metadata_validator import MetadataAnalyzer
+from metadata_parser import Parser
 
 
-def parse_package_metadata(metadata):
-    parsed_metadata = {}
-
-    """
-    keys = []
-    for key in metadata["last_serial"]:
-    	keys.append(key)
-    """
-
-    print(metadata["last_serial"])
-    return metadata
-
-
-def is_package_installed(package_name):
-    try:
-        importlib.import_module(package_name)
-        return True
-    except ModuleNotFoundError:
-        return False
-
-
-def get_package_metadata(package_name):
-    if not is_package_installed(package_name):
-        return None
-
-    metadata = importlib.metadata.metadata(package_name)
-    return metadata
-
-
-def fetch_npm_package_metadata(package_name):
-    """
-    Fetches package metadata from the npm repository.
-
-    Args:
-      package_name: The name of the package to fetch metadata for.
-
-    Returns:
-      A dictionary containing the package metadata, or None if the package is not found.
-    """
-
-    response = requests.get(f"https://registry.npmjs.org/{package_name}")
-    if response.status_code == 200:
-        print(response.json())
-        return response.json()
-    else:
-        return None
-'''
+repo_list = {
+1: 'GEMS_REPO',
+2: 'NPM_REPO',
+3: 'PYTHON_REPO'
+}
 
 if __name__ == "__main__":
     # TODO - Parse file, get repo type
-    repo = input("Enter the repository name:")
+    print('The following packages are supported for analysis...')
+    print('1. Gems Package')
+    print('2. NPM Package')
+    print('3. Python Package')
+    
+    repo = int(input("Enter the number for the relevant package:"))
     package_name = input("Enter the package name: ")
-
-    metadata = MetadataInfo.IsPackageInstalled(package_name,repo)
-    print(metadata)
+    
+    parser = Parser(package_name,repo_list[repo])
+    remote_pkg_metadata = parser.FetchAndParseRemote()
+    local_pkg_metadata = parser.FetchAndParseLocal()
+    print(local_pkg_metadata)	    
     exit(1)
-    parsed_metadata = parse_package_metadata(metadata)
-
+    b_metadata_analysis_res = ''
+    attack_analysis_res = ''
     # call metadata analysis here
-
-    is_installed = is_package_installed(package_name)
-    if is_installed:
-        context_metadata = get_package_metadata(package_name)
+    b_metadata_analysis_res = MetadataAnalyzer.MetadataAnalyzer(remote_pkg_metadata,local_pkg_metadata,repo)
+    for key,value in b_metadata_analysis_res.items():
+        if value is False:
+            print(f'Mismatch between local {key} and remote {key} metadata fields')
+    
+    print('Passed metadata check')
+    exit(1)
+    if Parser.IsPackageInstalled(package_name,repo):
+        local_pkg_metadata = 'get and parse local metadata'
         print("Successfully fetched local copy and remote copy...")
-        print(context_metadata)
+        exit(1)
         # call attack analysis with context_metadata
     else:
-        print(
-            f"The package {package_name} is not installed. Reverting back to popularity metrics..."
-        )
+        print(f"The package {package_name} is not installed. Reverting back to popularity metrics...")
         # call attack analysis with nonetype
-        res = analyze_package_metadata(metadata, "Python")
-        print(res)
+        #res = analyze_package_metadata(metadata, "Python")
+        #print(res)
+        exit(1)
