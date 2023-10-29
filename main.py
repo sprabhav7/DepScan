61f9466e89eccb5e58af23c3dc881abbd771743e
Prabhav <122600613+sprabhav7@users.noreply.github.com>
2023-10-28 23:57:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import importlib
 import importlib.metadata
 import requests
+import json
 from bs4 import BeautifulSoup
-from metadata_validator import MetadataAnalyzer
-from metadata_parser import Parser
+from MetadataAnalyzer import MetadataAnalyzer
+from ParseUtil import Parser
 
 
 repo_list = {
@@ -13,7 +14,7 @@ repo_list = {
 }
 
 if __name__ == "__main__":
-    # TODO - Parse file, get repo type
+    
     print('The following packages are supported for analysis...')
     print('1. Gems Package')
     print('2. NPM Package')
@@ -25,26 +26,19 @@ if __name__ == "__main__":
     parser = Parser(package_name,repo_list[repo])
     remote_pkg_metadata = parser.FetchAndParseRemote()
     local_pkg_metadata = parser.FetchAndParseLocal()
-    print(local_pkg_metadata)	    
-    exit(1)
+    
     b_metadata_analysis_res = ''
     attack_analysis_res = ''
-    # call metadata analysis here
-    b_metadata_analysis_res = MetadataAnalyzer.MetadataAnalyzer(remote_pkg_metadata,local_pkg_metadata,repo)
-    for key,value in b_metadata_analysis_res.items():
-        if value is False:
-            print(f'Mismatch between local {key} and remote {key} metadata fields')
     
-    print('Passed metadata check')
-    exit(1)
-    if Parser.IsPackageInstalled(package_name,repo):
-        local_pkg_metadata = 'get and parse local metadata'
+    if local_pkg_metadata:
         print("Successfully fetched local copy and remote copy...")
-        exit(1)
+        b_metadata_analysis_res = MetadataAnalyzer.Analyzer(remote_pkg_metadata,local_pkg_metadata,repo)
+        print(b_metadata_analysis_res)
         # call attack analysis with context_metadata
+        
     else:
-        print(f"The package {package_name} is not installed. Reverting back to popularity metrics...")
+        print(f"The package {package_name} is not installed. Reverting back to online popularity metrics...")
+        b_metadata_analysis_res = MetadataAnalyzer.Analyzer(remote_pkg_metadata,None,repo)
+        print(b_metadata_analysis_res)
         # call attack analysis with nonetype
-        #res = analyze_package_metadata(metadata, "Python")
-        #print(res)
         exit(1)
