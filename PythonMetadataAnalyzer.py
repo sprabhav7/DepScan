bc9f7a3de014fde6ca714234950ec04f2bdb0fb5
Prabhav <122600613+sprabhav7@users.noreply.github.com>
2023-10-30 17:49:07 -0400

    Update PythonMetadataAnalyzer.py

--- PythonMetadataAnalyzer.py
+++ PythonMetadataAnalyzer.py
@@ -4,8 +4,11 @@ import requests
 import hashlib
 import pkg_resources
 import pkgutil
+import os
+import datetime
 from bs4 import BeautifulSoup
 
+separator = '---------------------------------------------------------------------------'
 
 class PythonMetadataAnalyzer:
 	def __init__(self, remote_metadata, local_metadata):
@@ -15,82 +18,174 @@ class PythonMetadataAnalyzer:
 		self.res = {}
 	
 	def analyze(self):
+		print(separator)
 		self.AnalyzeBaseFeatureSet()
-		self.AnalyzeVersions()
-		self.AnalyzeAuthor()
-		self.AnalyzeMaintainers()
-		'''self.AnalyzeTypoSquatting()
-		self.AnalyzeDependencyConfusionAttack()
-		
-		if local_metadata is None:
+			
+		if self.local_metadata is None:
+			print('Analyzing package with local metadata data as context...')
+			print(separator)
+			
 			self.AnalyzePopularityMetrics()
-			self.AnalyzePackageAge()
-			self.ValidateProjectURL()
-			self.AnalyzeImmaturePackage()
-			self.AnalyzeFirstPackage()
-			self.MissingVersions()
+			self.AnalyzeMissingVersions()
+			self.AnalyzeIncreasingVersions()
+			self.AnalyzePackageURL()
 			
 
 		else:
+			print('Reverting to public popularity metrics of the package...')
+			print(separator)
+			
 			self.AnalyzeVersions()
-			self.ValidateDependencies()
-			self.AnalyzeAuthors()
+			self.AnalyzeAuthor()
 			self.AnalyzeMaintainers()
-			self.ValidateLocalChecksum()
-			self.AnalyzeProjectURL()'''
+			self.AnalyzeProjectURL()
+			self.AnalyzeSummary()
+			self.AnalyzeLicense()
+			self.AnalyzeContextAge()
+			self.AnalyzeDependencies()
+			
 		return self.res
 			
 	def AnalyzeBaseFeatureSet(self):
+		print('Analyzing base features...')
+		print(separator)
 		rem_keys = [self.remote_metadata.keys() - self.keys]
 		
 		if not self.local_metadata is None:
 			for key in self.keys:
 				if self.remote_metadata[key] is None:
 					if not self.local_metadata[key] is None:
-						self.res.update({key:{'ALERT':f'Values are different between local and remote packages.'}})
+						self.res.update({key:{'WARN':f'AnalyzeBaseFeatureSet:Mismatch between local and remote packages.'}})
 		else:
 			for key in self.keys:
 				if self.remote_metadata[key] is None:
-					self.res.update({key:{'WARN':f'A security metadata feature that does not have a value.'}}) 
+					self.res.update({key:{'WARN':f'AnalyzeBaseFeatureSet:A security metadata feature that does not have a value.'}}) 
 	
 	def AnalyzeVersions(self):
+		print('Analyzing versions based on context...')
+		print(separator)
 		if self.local_metadata['version'] and not self.local_metadata['version'] in self.remote_metadata['versions'].keys():
-			self.res.update({'version':{'ALERT':f'Remote package does not have {self.local_metadata["version"]} in the list of versions.'}}) 
+			self.res.update({'version':{'ALERT':f'AnalyzeVersions:Version mismatch between local and remote packages'}}) 
 			
 	def AnalyzeAuthor(self):
-		if self.local_metadata['author'] != self.remote_metadata['author']:
-			self.res.update({'author':{'ALERT':f'Remote package : {self.remote_metadata["author"]} ; Local package : {self.local_metadata["author"]}.'}}) 
+		print('Analyzing authors based on context...')
+		print(separator)
+		if self.local_metadata['author'] != self.remote_metadata['author'] and not self.local_metadata['author'] in self.remote_metadata['author-email']:
+			self.res.update({'author':{'ALERT':f'AnalyzeAuthor:Author mismatch between local and remote packages'}}) 
 		
-		if self.local_metadata['author-email'] != self.remote_metadata['author-email']:
-			self.res.update({'author-email':{'ALERT':f'Remote package : {self.remote_metadata["author-email"]} ; Local package : {self.local_metadata["author-email"]}.'}}) 
+		if self.local_metadata['author-email'] != self.remote_metadata['author-email'] and self.local_metadata['author-email'] not in self.remote_metadata['author-email']:
+			self.res.update({'author-email':{'ALERT':f'AnalyzeAuthor:Author email mismatch between local and remote packages'}}) 
 	
 	def AnalyzeMaintainers(self):
-		if self.local_metadata['maintainer'] != self.remote_metadata['maintainer']:
-			self.res.update({'maintainer':{'ALERT':f'Remote package : {self.remote_metadata["maintainer"]} ; Local package : {self.local_metadata["maintainer"]}.'}})
+		print('Analyzing maintainers based on context...')
+		print(separator)
+		if self.local_metadata['maintainer'] != self.remote_metadata['maintainer'] and self.local_metadata['maintainer'] not in self.remote_metadata['maintainer-email']:
+			self.res.update({'maintainer':{'ALERT':f'AnalyzeMaintainers:Maintainer mismatch between local and remote packages'}})
 			
 		if self.local_metadata['maintainer-email'] != self.remote_metadata['maintainer-email']:
-			self.res.update({'maintainer-email':{'ALERT':f'Remote package : {self.remote_metadata["maintainer-email"]} ; Local package : {self.local_metadata["maintainer-email"]}.'}}) 
-	'''
-	def ValidateLocalChecksum(self):
-		pkg_sha256 = self.remote_metadata['versions'][self.local_metadata['version']][0]['digests']['sha256']
-		print(pkg_sha256)
-		exit(1)
-		if 'version' in self.res.keys():
-			 #do some errors stuff
-			 return
-			 
-		package_data = pkgutil.get_data(self.local_metadata['name'], "__init__.py")
-		if package_data is None:
-			raise FileNotFoundError("Package not found or has no '__init__.py' file")
+			self.res.update({'maintainer-email':{'ALERT':f'AnalyzeMaintainers:Maintainer Email mismatch between local and remote packages'}}) 
+	
+	def AnalyzeProjectURL(self):
+		print('Analyzing project url based on context...')
+		print(separator)
+		if self.local_metadata['project-url'] != self.remote_metadata['project-url']:
+			self.res.update({'maintainer':{'ALERT':f'AnalyzeProjectURL: Project URL mismatch between local and remote packages'}})
+	
+	
+	def AnalyzeSummary(self):
+		print('Analyzing summary based on context...')
+		print(separator)
+		if self.local_metadata['summary'] != self.remote_metadata['summary']:
+			self.res.update({'summary':{'ALERT':f'AnalyzeSummary:Summary mismatch between local and remote packages'}})
+			
+	def AnalyzeLicense(self):
+		print('Analyzing license based on context...')
+		print(separator)
+		if self.local_metadata['license'] != self.remote_metadata['license'] and self.local_metadata['license'][:3] not in self.remote_metadata['license']:
+			self.res.update({'license':{'ALERT':f'AnalyzeLicense:License mismatch between local and remote packages'}})
+	
+	def AnalyzeContextAge(self):
+		print('Analyzing age based on context...')
+		print(separator)
+		try:
+			distribution = pkg_resources.get_distribution(self.local_metadata['name'])
+			distribution_location = distribution.location
+			package_path = os.path.join(distribution_location, self.local_metadata['name'])
+			creation_timestamp = os.path.getctime(package_path)
+			install_date = str(datetime.datetime.fromtimestamp(creation_timestamp).date())+"T"+str(datetime.datetime.fromtimestamp(creation_timestamp).time()).split('.')[0]
+			
+			if self.local_metadata['version'] in self.res.keys():
+				self.res.update({'age':{'ALERT':f'AnalyzeContextAge:Unable to validate timestamp due to version mismatch'}})
+				
+			release_date = self.remote_metadata['versions'][self.local_metadata['version']][0]['upload_time']
+			
+			
+			install_date = datetime.datetime.strptime(install_date, '%Y-%m-%dT%H:%M:%S')
+			release_date = datetime.datetime.strptime(release_date, '%Y-%m-%dT%H:%M:%S')
+			
+			
+			if release_date > install_date:
+				self.res.update({'age':{'ALERT':f'AnalyzeContextAge:Remote package has a timestamp greater than '}})
+		except:
+			self.res.update({'age':{'WARN':f'AnalyzeContextAge:Unable to fetch Local Metadata timestamps'}})
+			
+	def AnalyzeDependencies(self):
+		print('Analyzing license based on context...')
+		print(separator)
+		if self.local_metadata['dependencies'] != self.remote_metadata['dependencies'] and self.local_metadata['dependencies'] not in self.remote_metadata['dependencies']:
+			self.res.update({'license':{'ALERT':f'AnalyzeDependencies: Dependency mismatch between local and remote packages'}})
+	
+	def AnalyzePopularityMetrics(self):
+		print('Analyzing popularity...')
+		print(separator)
+		api_url = f'https://pypistats.org/api/packages/{self.remote_metadata["name"]}/recent'
+		response = requests.get(api_url)
+		if response.status_code == 200:
+			downloads = response.json()['data']
+			week_download_stats = downloads['last_week']
+			month_download_stats = downloads['last_month']
+			day_download_stats = downloads['last_day']
+			
+			# check the three values
+			if day_download_stats == week_download_stats and week_download_stats == month_download_stats:
+				self.res.update({'popularity':{'WARN':f'AnalyzePopularityMetrics: Package is new, recommended validation against attack patterns'}})
+			
+			if int(week_download_stats) < 10000:
+				self.res.update({'popularity':{'ALERT':f'AnalyzePopularityMetrics: Package below threshold for downloads, recommended validation against attack patterns'}})
+		else:
+			return None		
+	
+	def AnalyzeMissingVersions(self):
+		print('Analyzing missing versions...')
+		print(separator)
 		
-		hash_algorithm = hashlib.sha256()  # You can choose a different hashing algorithm
-		hash_algorithm.update(package_data)
-		calculated_hash = hash_algorithm.hexdigest()
-		print(calculated_hash)
-	'''	
+		versions = list(map(lambda x : int(x.split('.')[0]),list(self.remote_metadata['versions'].keys())))
+		skipped_list = []
+		for i in range(max(versions)):
+			if i not in versions:
+				skipped_list.append(i)
+		
+		if skipped_list:
+			self.res.update({'missing-versions':{'ALERT':f'AnalyzeMissingVersions: Versions {skipped_list} have been skipped'}})
+			
+	def AnalyzeIncreasingVersions(self):
+		print('Analyzing strictly increasing versions...')
+		print(separator)
+		
+		versions = list(map(lambda x : float(x.split('.')[0]+"."+x.split('.')[1]),list(self.remote_metadata['versions'].keys())))
+		
+		for i in range(len(versions)-1):
+			if versions[i+1]-versions[i] < 0:
+				self.res.update({'increasing-versions':{'ALERT':f'AnalyzeIncreasingVersions: Metadata versions are not strictly increasing: {versions[i+1]}, {versions[i]}'}})
+				break
 	
-	def AnalyzeTypoSquatting(self, package_name):
-		return 'Hello World'
-	
-	def AnalyzeDependencyConfusion(self,package_name):
-		return 'Hello World'
+	def AnalyzePackageURL(self):
+		print('Analyzing certificate validity of URL...')
+		print(separator)
+		
+		response = requests.head(self.remote_metadata['project-url'])
+		response.raise_for_status()
+
+		# Verify the SSL/TLS certificate
+		if not response.ok:
+			self.res.update({'project-url':{'ALERT':f'AnalyzePackageURL: Invalid certificate detected for the package'}})
