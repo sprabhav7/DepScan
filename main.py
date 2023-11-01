a052e1de483f338b08d501fa05248f630f7c1d80
Prabhav <122600613+sprabhav7@users.noreply.github.com>
2023-11-01 01:58:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import importlib.metadata
 import requests
 import json
 import os
+import sys
 from bs4 import BeautifulSoup
 from MetadataAnalyzer import MetadataAnalyzer
 from ParseUtil import Parser
@@ -16,18 +17,29 @@ repo_list = {
 
 if __name__ == "__main__":
     
-    file_path = input("Enter the absolute path to the packages file:\n")
+    file_path = sys.argv[1]
     
     if not os.path.exists(file_path):
         print("Unable to find the file, please retry..")
         exit(1)
-    
+    '''
     print('The following packages are supported for analysis...')
     print('1. Gems Package')
     print('2. NPM Package')
     print('3. Python Package')
+    '''
     
-    repo = int(input("Enter the number for the relevant packge for analysis:"))
+    repo = int(sys.argv[2])
+    
+    if repo == 1:
+    	print('Initializing Gems Analyzer...\n')
+    elif repo == 2:
+    	print('Initializing NPM Analyzer...\n')
+    elif repo == 3:
+    	print('Initializing Python Analyzer...\n')
+    else:
+    	print('Invalid selection, try again...\n')
+    	exit(1)
     
     with open(file_path,"r") as f:
     	packages = f.readlines()
@@ -40,12 +52,10 @@ if __name__ == "__main__":
         local_pkg_metadata = parser.FetchAndParseLocal()
         print('Analyzing package for issues related to metadata\n\n')
         '''with open('npm_remote_md','w') as f:
-            for item in list(remote_pkg_metadata.keys()):
-            	f.write(item+'\n')
+            json.dump(remote_pkg_metadata,f,indent=4)
         
         with open('npm_local_md','w') as f:
-            for item in list(local_pkg_metadata.keys()):
-            	f.write(item+'\n')
+            json.dump(local_pkg_metadata,f,indent=4)
         
         break'''
         b_metadata_analysis_res = ''
@@ -55,4 +65,5 @@ if __name__ == "__main__":
         else:
             b_metadata_analysis_res = MetadataAnalyzer.Analyzer(remote_pkg_metadata,None,repo)
         
-        print(json.dumps(b_metadata_analysis_res))
+        print(b_metadata_analysis_res)
+        print('\n')
