b05111d106bf4483e7930742dcb1970e4f6c987c
Prabhav <122600613+sprabhav7@users.noreply.github.com>
2023-11-11 03:25:51 -0500

    Update DependencyConfusionAnalyzer.py

--- DependencyConfusionAnalyzer.py
+++ DependencyConfusionAnalyzer.py
@@ -19,13 +19,12 @@ safe_repo_list = {
 
 class DependencyConfusionAnalyzer:
 	def __init__(self, remote_metadata,repo):
-		print('Starting dependency confusion analyzer\n')
+		print('Starting dependency confusion analyzer')
 		self.remote_metadata= remote_metadata
 		self.trusted_metadata = None
 		self.repo = repo
-		self.res = {}
+		self.res = {'WARN': [], 'ALERT': [], 'FATAL': []}
 		self.security_features = ['version', 'summary', 'author', 'dependencies', 'project-url', 'versions']
-		logging.basicConfig(level=logging.DEBUG)
 	
 	def analyze(self):
 		self.FetchFromTrusted()
@@ -47,7 +46,7 @@ class DependencyConfusionAnalyzer:
 	
 	def ValidatePackageDetails(self):
 		if self.trusted_metadata is None:
-			self.res.update({'WARN':'Unable to find package in public repository, consider reserving this namespace in a public repository'})
+			self.res['ALERT'].append('Unable to find package in public repository, consider reserving this namespace in a public repository')
 		else:
 			count = 0
 			for key in self.security_features:
@@ -55,13 +54,6 @@ class DependencyConfusionAnalyzer:
 					count +=1
 			
 			if count <= len(self.security_features)/2 and count > 0:
-				self.res.update({'WARN':'Possible dependency confusion attack detected. Review and update packages'})
+				self.res['WARN'].append('Possible dependency confusion attack detected. Review and update packages')
 			elif count > len(self.security_features)/2:
-				self.res.update({'ALERT':'Dependency confusion attack detected. Removing package from download list'})
-				
-				
-			
-
-		
-		
-			
+				self.res['ALERT'].append('Dependency confusion attack detected. Removing package from download list')
